93dd62fb0190d4a2e99716394673f586aa845c5a
Yechen <yechen.li@37-int.com>
2014-10-15 16:13:59 +0800

    table array

--- Lua01/src/main.lua
+++ Lua01/src/main.lua
@@ -1,1 +1,37 @@
-print("hello lua")
+--print("hello lua")
+
+--table
+Config = {hello = "hello world"};
+Config.words = "hello"
+Config.num = 100;
+Config["name"] = "zhang san"
+
+--print(Config["words"])
+--print(Config.name)
+--print(Config.hello)
+--
+--for key, var in pairs(Config) do
+--	print(key,var)
+--end
+
+
+--array
+
+--arr = {1,2,3,4,"teststr",5,6}
+--
+--table.maxn(arr)
+
+--for key, var in pairs(arr) do
+--	print(key,var)
+--end
+
+arr = {};
+for var=1, 100 do
+  table.insert(arr,1,var)
+	
+end
+
+for key, var in pairs(arr) do
+  print(key,var)
+end
+
